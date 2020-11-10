e7d6a9c9f894a5325825f8989e017f2c0c8bbbeb
carlos sanchez-valero <carlossanchez-valero@MacBook-Pro-de-carlos.local>
2020-11-11 00:31:55 +0100

    Añadimos la lista baraja como variable local de la funcion creaBaraja

--- baraja.py
+++ baraja.py
@@ -3,16 +3,11 @@ numeros = ['A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R']
 #creamos lista todos los palos de la baraja
 palos = ['o', 'c', 'e', 'b']
 
-baraja = []
-
 #Tenemos que concatenar las cartas con los palos para generar la baraja
-def Crearbaraja ():
+def crearBaraja ():
+    baraja = []
     for palo in palos:
         for numero in numeros:
             baraja.append(numero+palo)
     
     return baraja
-
-Crearbaraja()
-    
-
