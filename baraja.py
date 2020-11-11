340ede4210b8f3cf1b3c07002c3c0b6436217d9d
carlos sanchez-valero <carlossanchez-valero@MacBook-Pro-de-carlos.local>
2020-11-11 22:32:25 +0100

    Funcion Barajar

--- baraja.py
+++ baraja.py
@@ -1,3 +1,5 @@
+import random
+
 #creamos lista con todas las cartas
 numeros = ['A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R']
 #creamos lista todos los palos de la baraja
@@ -11,3 +13,29 @@ def crearBaraja ():
             baraja.append(numero+palo)
     
     return baraja
+
+#Esta función nos ayuda a entender como intercambiar valores dentro de una lista
+'''
+def intercambio (v1,v2):
+    aux = v1
+    v1 = v2
+    v2 = aux
+
+    return v1, v2
+'''
+
+#Definimos una función que pueda intercambiar posiciones en cualquier lista.
+def barajar (lista_de_cartas):
+    #Poniendo in range conseguimos iterar en los indices de la lista (hasta la longitud de la lista), no los valores.
+    for i in range(len(lista_de_cartas)):
+        #usando el random.range creamos un valor de posicion nuevo que luego intercambiaremos con la i que vamos iterando
+        nueva_posicion = random.randrange (len(lista_de_cartas))
+
+        aux = lista_de_cartas[nueva_posicion]
+        lista[_de_cartas[nueva_posicion] = lista_de_cartas [i]
+        lista_de_cartas [i] = aux
+    
+
+        
+
+    
